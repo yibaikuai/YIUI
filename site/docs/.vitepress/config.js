59d11ef0d468500bb414eb3f481810f526dc7d2d
yibaikuai <2276001806@qq.com>
2023-03-26 00:01:35 +0800

    [docs]:初始化文档

---
+++ site/docs/.vitepress/config.js
@@ -0,0 +1,50 @@
+export default {
+    title: 'yibaikuai-ui',
+    // base: process.env.NODE_ENV === 'production' ? '/easyest/' : '/',
+    themeConfig: {
+        nav: [{ text: '文档', link: '/guild/introduce' },
+            { text: "指南", link: "/guild/installation" },
+            { text: "组件", link: "/components/button/" },],
+        sidebar: {
+            '/': [
+                {
+                    text: '引入',
+                    items: [
+                        {
+                            text: '介绍',
+                            link: '/guild/introduce'
+                        },
+                        {
+                            text: '快速开始',
+                            link: '/guild/quickstart'
+                        }
+                    ]
+                },
+                {
+                    text: '开发',
+                    items: [
+                        {
+                            text: '目录结构',
+                            link: '/develop/'
+                        },
+                        {
+                            text: '组件开发',
+                            link: '/develop/component'
+                        },
+                        {
+                            text: '全局组件',
+                            link: '/develop/global'
+                        },
+
+                        {
+                            text: '打包发布',
+                            link: '/develop/build'
+                        }
+                    ]
+                }
+            ]
+        },
+
+        socialLinks: [{ icon: 'github', link: 'https://github.com/yibaikuai/yibaikuai-ui' }]
+    }
+};
